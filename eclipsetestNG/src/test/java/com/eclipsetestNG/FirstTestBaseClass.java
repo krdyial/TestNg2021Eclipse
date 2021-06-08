6e363b34b295a211509930fed27a8959ad20b8ec
Karadayi <aldimverdim48@gmail.com>
2021-06-09 00:31:55 +0200

    a

--- eclipsetestNG/src/test/java/com/eclipsetestNG/FirstTestBaseClass.java
+++ eclipsetestNG/src/test/java/com/eclipsetestNG/FirstTestBaseClass.java
@@ -12,6 +12,11 @@ public class FirstTestBaseClass extends TestBase {
 	@Test
 	public void test1() {
 		driver.get("https://www.google.com");
+		try {
+			driver.findElement(By.)
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 	}
 
 }
